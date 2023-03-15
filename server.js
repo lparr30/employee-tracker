16491eb02a042c9dc5cb866c6d9fd112fdc3e044
Liza Parr <lizajparr@gmail.com>
2023-03-15 15:15:51 -0400

    modified choices

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
+// const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
-const app = expess();
+const app = express();
 
 // middleware
 app.use(express.json());
@@ -20,6 +20,35 @@ const db = mysql.createConnection(
         database: 'company_db'
     }
 )
+
+// PROMPTING QUESTIONS
+inquirer.prompt([
+    {
+        type: "list",
+        message: "What would you like to do?",
+        name: "task",
+        choices: [ "View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role" ]
+    }
+])
+
+.then((response) => {
+    const task = response.task;
+    if (task === "View all departments"){
+        db.query('SELECT * FROM department;', function(err, results) {
+            console.table(results);
+        });
+    }
+    else if(task === "View all roles"){
+        db.query('SELECT * FROM role;', function (err, results) {
+            console.table(results);
+        });
+    }
+    else {
+        db.query('SELECT * FROM employees;', function (err, results) {
+            console.table(results);
+        })
+    }
+})
 
 // ROUTES
 
