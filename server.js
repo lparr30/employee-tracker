ed2fd9cd6ffa0de67cd68b52486f48cc8d678d29
Liza Parr <lizajparr@gmail.com>
2023-03-16 23:05:17 -0400

    final changes

--- server.js
+++ server.js
@@ -74,15 +74,97 @@ inquirer.prompt([
             {
                 type:"input",
                 message: "What is the title of the role?",
-                name: "role"
+                name: "role-title"
+            },
+            {
+                type:"input",
+                message: "What is the salary of the role?",
+                name: "role-salary"
+            }, 
+            {
+                type:"list",
+                message: "What department does your role belong to?",
+                name: "role-dept",
+                choices: ["Boss, 1", "R & D, 2", "Science, 3", "HR, 4", "Reception, 5"]
             }
         ])
         .then((reply) => {
-            const role = reply.role
-            db.query('INSERT INTO role (titleL VALUES (?);', [role], function(err, results) {
-                if(err) {
+            const title = reply['role-title']
+            const salary = reply['role-salary']
+            const department_ID = reply['role-dept'].split(', ')[1]
+            db.query('INSERT INTO role SET ?', {title, salary, department_ID, function(err, results) {
+                if (err) {
                     throw err;
                 }
+            }})
+            db.query('SELECT * FROM role;', function (err, results) {
+                console.table(results);
+            });
+        })
+    } else if(task === "Add an employee"){
+        inquirer.prompt([
+            {
+                type:"input",
+                message: "What is the employee's first name?",
+                name: "first_name"
+            },
+            {
+                type:"input",
+                message: "What is the employee's last name?",
+                name: "last_name"
+            }, 
+            {
+                type:"list",
+                message: "What is your employee's role?",
+                name: "role_id",
+                choices: ["CEO, 1", "Inventor, 2", "Lab Tech, 3", "Talent Acquisition, 4", "Receptionist, 5"]
+            },
+            {
+                type:"list",
+                message: "Who is your employee's manager?",
+                name: "manager_id",
+                choices: ["Trixie Mattel, 1", "Santa Claus, 2"]
+            }
+        ])
+        .then((reply) => {
+            const first_name = reply.first_name
+            const last_name = reply.last_name
+            const role_id = reply.role_id.split(', ')[1]
+            const manager_id = reply.manager_id.split(', ')[1]
+            db.query('INSERT INTO employee SET ?', {first_name, last_name, role_id, manager_id, function(err, results) {
+                if (err) {
+                    throw err;
+                }
+            }})
+            db.query('SELECT * FROM employee;', function (err, results) {
+                console.table(results);
+            });
+        })
+    } else if(task === "Update an employee role") {
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Which employee do you want to update the role for?",
+                name: "id",
+                choices: ["Trixie Mattel, 1", "Santa Clause, 2", "Bill Nye, 3", "Jenna Marbles, 4", "Fran Fine, 5"]
+            },
+            {
+                type: "list",
+                message: "What is the new role?",
+                name: "role_id",
+                choices: ["CEO, 1", "Inventor, 2", "Lab Tech, 3", "Talent Acquisition, 4", "Receptionist, 5"]
+            }
+        ])
+        .then((reply) => {
+            const id = reply.id.split(', ')[1]
+            const role_id = reply.role_id.split(', ')[1]
+            db.query('UPDATE employee SET role_id = ? WHERE id = ?', [role_id, id], function(err, results) {
+                if (err) {
+                    throw err;
+                }
+            })
+            db.query('SELECT * FROM employee;', function (err, results) {
+                console.table(results);
             });
         })
     }
